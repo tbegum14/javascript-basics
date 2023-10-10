c6253eb5643409b9bf923977b5e59cfffed94f16
Thasnima Begum <thasnima@hotmail.co.uk>
2023-10-10 10:52:51 +0100

    string tests complete

--- src/strings.js
+++ src/strings.js
@@ -20,10 +20,12 @@ function countCharacters (string) {
 
 function firstCharacter (string) {
   // your code here
+  return string.charAt(0);
 };
 
 function firstCharacters (string, n) {
   // your code here
+  return string.substring(0, n);
 };
 
 module.exports = {
