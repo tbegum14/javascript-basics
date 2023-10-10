2c0cb9f68d8130a3d8c07e885b575b4920fd6c72
Thasnima Begum <thasnima@hotmail.co.uk>
2023-10-10 15:47:30 +0100

    boolean tests complete

--- src/booleans.js
+++ src/booleans.js
@@ -20,46 +20,59 @@ function none(a, b) {
 
 function one(a, b) {
   // your code here
+  return (a&&!b || !a&b)? true:false;
 };
 
 function truthiness(a) {
   // your code here
+  return a? true:false;
 };
 
 function isEqual(a, b) {
   // your code here
+  return a===b? true:false;
 };
 
 function isGreaterThan(a, b) {
   // your code here
+  return a>b? true:false;
 };
 
 function isLessThanOrEqualTo(a, b) {
   // your code here
+  return a<=b? true:false;
 };
 
 function isOdd(a) {
   // your code here
+  return a%2!=0? true:false;
 };
 
 function isEven(a) {
   // your code here
+  return a%2===0? true:false;
 };
 
 function isSquare(a) {
   // your code here
+  return Number.isInteger(Math.sqrt(a))? true:false;
+
 };
 
 function startsWith(char, string) {
   // your code here
+  return string.charAt(0) === char? true:false;
 };
 
 function containsVowels(string) {
   // your code here
+  return /[aeiou]/gi.test(string);
 };
 
 function isLowerCase(string) {
   // your code here
+  return !/[ABDCDEFGHIJKLMNOPQRSTUVWXYZ]/g.test(string);
+
 };
 
 module.exports = {
