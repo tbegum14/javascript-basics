83c3469489ff1c4d44f598755e5c924cae28a2b1
Thasnima Begum <thasnima@hotmail.co.uk>
2023-10-11 10:32:31 +0100

    array tests in progress

--- src/arrays.js
+++ src/arrays.js
@@ -1,29 +1,42 @@
 const getNthElement = (index, array) => {
   // your code here
+  while (index>=array.length){
+    index = index - array.length
+  }
+  return array[index];
 };
 
 const arrayToCSVString = array => {
   // your code here
+  return array.toString()
 };
 
 const csvStringToArray = string => {
   // your code here
+  return string.split(',')
 };
 
 const addToArray = (element, array) => {
   // your code here
+  array.push(element)
 };
 
 const addToArray2 = (element, array) => {
   // your code here
+  return array.concat(element)
+ 
+
 };
 
 const removeNthElement = (index, array) => {
   // your code here
+  return array.splice(index, 1)
+
 };
 
 const numbersToStrings = numbers => {
   // your code here
+  return numbers.join().split(',')
 };
 
 const uppercaseWordsInArray = strings => {
