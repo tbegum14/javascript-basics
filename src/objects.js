040f2b6e40a2e738ef3e92945f35f0568c836cff
Thasnima Begum <thasnima@hotmail.co.uk>
2023-10-19 16:24:37 +0100

    object tests in progress

--- src/objects.js
+++ src/objects.js
@@ -1,37 +1,64 @@
 const createPerson = (name, age) => {
   // your code here
+  return {"name": name, "age": age}
 };
 
 const getName = object => {
   // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
   // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
   // your code here
+  return object[property]? true: false;
 };
 
 const isOver65 = person => {
   // your code here
+  return person.age>65? true:false;
 };
 
 const getAges = people => {
   // your code here
+  const array=[]
+  for (let i=0; i<people.length;i++){
+    array.push(people[i].age)
+  }
+  return array
 };
 
 const findByName = (name, people) => {
   // your code here
+for (let i=0;i<people.length;i++){
+  if (people[i].name === name){
+    return people[i]
+  }
+}
 };
 
 const findHondas = cars => {
   // your code here
+  const array = []
+  for (let i=0;i<cars.length;i++){
+    if (cars[i].manufacturer === "Honda"){
+      array.push(cars[i])
+    }
+    return array
+  }
 };
 
 const averageAge = people => {
   // your code here
+  const array=[]
+  for (let i=0;i<people.length;i++){
+    array.push(people[i].age)
+  }
+  return array.reduce((a,b)=>a+b, 0)/array.length
 };
 
 const createTalkingPerson = (name, age) => {
